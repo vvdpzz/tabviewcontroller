f5a9a1ec528a3b943911ed424fd420b2543076bf
feipinghuang <huang900107@163.con>
2011-11-24 20:49:16 +0800

    Good job

---
+++ app/assets/javascripts/tableViewController.js
@@ -0,0 +1,40 @@
+/* ============================================================
+ * 
+ * ============================================================ */
+
+!function( $ ){
+  fetch = function(template, el, page, url) {
+    $.get(url, {page: page}, function(data, textStatus, xhr) {
+      if (page == 1){
+        $(el).append(template(data));
+        // generate load-more button
+        var loadMore = $("<button/>").attr({class: "load-more", "data-page": 1 }).text("Load More...");
+        $(el).append(loadMore);
+
+        // bind load-more click event
+        $(el).find(".load-more").live("click", function(){
+          data = $(this).data();
+          fetch(template, el, ++data.page, url);
+        });
+      } else {
+        for(i in data){
+          if (data[i].length > 0){
+            $(el).find(".load-more").before(template(data));
+          } else {
+            $(el).find(".load-more").remove();
+          }
+          break;
+        } 
+      }
+    });
+  };
+  $.fn.tableViewController = function(options) {
+    // compile the particular handlebar template
+    var template = Handlebars.compile($(options.template).html());
+    
+    // fetch the initialize data
+    fetch(template, this, 1, options.dataSource);
+    
+    return this;
+  }
+}( window.jQuery );
